d50424dd2c0c5504a041484e5f42599b7474bda1
SunJin Yoon <56009413+ysjin94@users.noreply.github.com>
2020-12-01 16:35:38 -0500

    Update smart_contract.sol

--- smart_contract.sol
+++ smart_contract.sol
@@ -1,6 +1,9 @@
 pragma solidity 0.5.1;
 
 Contract Music{
+  // initial code is empty.
+  uint256 public codeCount = 0
+  
   // owner can only modify the contract
   address owner;
     
@@ -8,4 +11,22 @@ Contract Music{
       require(msg.sender == owner);
       _;
     }
+    
+    struct Code{
+      uint _code;
+    }
+    
+    constructor() public {
+      owner = msg.sender;
+    }
+    
+    function addCode(uint _code) public onlyOwner{
+      //increase total number of the codes
+      incrememtCodeCount();
+      Code[codecount] = Code(_code);
+    }
+    
+    function incrementCodeCount() internal{
+      codeCount += 1;
+      
 }
