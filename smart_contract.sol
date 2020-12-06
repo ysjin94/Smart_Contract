90493209e5e4f0cbdcc4dc0870c440b83e964106
SunJin Yoon <56009413+ysjin94@users.noreply.github.com>
2020-12-06 12:58:50 -0500

    Update smart_contract.sol

--- smart_contract.sol
+++ smart_contract.sol
@@ -1,54 +1,45 @@
 pragma solidity 0.5.1;
 
-contract owner{
-    address payable owner_wallet;
-    
-     constructor() public{
-        owner_wallet = msg.sender;
-    }
-    
-    modifier onlyOwner{
-        require(msg.sender == owner_wallet, "Only owner can modify the function");
-        _;
-    }
-    
-}
 
-contract deconstrcut is owner{
-    function destory() public onlyOwner {
-        selfdestruct(owner_wallet);
-    }
-}
-
-contract Music is deconstrcut{
-    
+contract Music {
     uint256 public codeCount = 0;
     mapping(uint256 => Code) public code;
 
+    address payable wallet;
+    
     constructor(address payable _wallet) public{
-        owner_wallet = _wallet;
+       wallet = _wallet;
     }
+    
+    modifier onlyOwner{
+        require(msg.sender == wallet, "Only owner can modify the function");
+        _;
+    }
+    
     
     struct Code{
-      uint _code;
-      string _id;
-      uint _cost;
-      uint manufacture_date;
-     
+      string _code;
     }
     
+    event InsertCode(
+        string _input,
+        uint256 _count
+        );
+    
     event OrderInfo(
-        string order_id,
-        address _buyer,
-        uint _code,
+        string indexed order_id,
+        address indexed _buyer,
+//        string _code,
         uint _cost,
         uint manufacture_date
     );
-     
-    function addCode(uint _code, string memory order_id, uint _cost ) public onlyOwner{
+
+    function addCode(string memory _code) public onlyOwner{
       //increase total number of the codes
       incrementCodeCount();
-      code[codeCount] = Code(_code, order_id, _cost, block.timestamp);
+      code[codeCount] = Code(_code);
+      emit InsertCode(_code, codeCount);
+      
     }
     
     
@@ -56,14 +47,22 @@ contract Music is deconstrcut{
       codeCount += 1;
     }
     
-    function buyCode() public payable{
-      // need to add show the music streaming code.
-      // and delete the code from the list.
-      // add discount, if the user buy the code several times, he get discount 50%
-      // after that the stack will be reset.
-      owner_wallet.transfer(msg.value);
+    function decrementCodeCount() internal{
+      codeCount -= 1;
+    }
+    
+    function buyCode(string memory _order_id) public payable{
+        
+        require(codeCount > 0, "It is sold out, please contact to Seller");
+        require(msg.value == 1, "It is not correct value, please put right value");
+        
+        wallet.transfer(msg.value);
+        emit OrderInfo(_order_id, msg.sender, 1, block.timestamp); //, code[codeCount], msg.value, block.timestamp);
+        
+        delete code[codeCount];
+        decrementCodeCount();
+      
       //emit OrderInfo( ,code[codeCount],msg.value, block.timestamp);
         
     }
- 
 }
