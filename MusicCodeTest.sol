de6ef0f7be7d2d541e665aaf010148c6fdfcfa5e
SunJin Yoon <56009413+ysjin94@users.noreply.github.com>
2020-12-06 21:38:20 -0500

    Update MusicCodeTest.sol

--- MusicCodeTest.sol
+++ MusicCodeTest.sol
@@ -1,1 +1,40 @@
+pragma solidity 0.5.16;
+
+import "truffle/Assert.sol";
+import "../contracts/MusicCode.sol";
+
+contract MusicCodeTest{
+	uint public initialBalance = 1 ether;
+		
+	function testSettingAnOwnerDuringCreation() public {
+		Music music = new Music();
+	}
+	
+	function testAddCode() public {
+		Music music = new Music();
+                music.addCode("ThisIsTest1");
+	}
+
+	function testBuyCode() public {
+		Music music = new Music();
+		music.addCode("ThisIsTest2");
+		music.buyCode("User_ID");
+	}
+	
+	function testBuyCode_SoldOut() public {
+		Music music = new Music();
+		music.buyCode("User_ID");
+	}
+
+	function testBuyCode_WrongValue() public{
+		Music music = new Music();
+		music.addCode("ThisIsTest3");
+		music.buyCode("User_ID");
+	}
+	
+	function TestClose() public{
+		Music music = new Music();
+		music.close();
+	}
+}
 
