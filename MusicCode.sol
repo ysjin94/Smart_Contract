fff3ccb1fc8555f3591bda6fe4f99c660a381b40
SunJin Yoon <56009413+ysjin94@users.noreply.github.com>
2020-12-08 16:00:36 -0500

    Update MusicCode.sol

--- MusicCode.sol
+++ MusicCode.sol
@@ -48,9 +48,16 @@ contract Music {
     );
     
     function close() public onlyOwner{
-        
         selfdestruct(wallet);
-        
+
+    }
+    
+    function clear() public onlyOwner{
+        for(uint i = 0; i <NumberOfBuyer; i++){
+            if( buyer[i].manufacture_date + 30 days < block.timestamp)
+                delete buyer[i];
+                
+        }
     }
     
     function addCode(string memory _code) public onlyOwner{
@@ -116,6 +123,8 @@ contract Music {
               return buyer[i]._code;
             
         }
+        
+        require(false, "we could not find your ID, it might be expired. please check the date you bought code. All ID will destory the date you bought after 30 days");
         
     }
 }
